84c4ce54ad2e055863b101c0c430b1dfd4dff1f7
hamidrgholipour <105815785+hamidrgholipour@users.noreply.github.com>
2023-12-08 11:32:29 +0330

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,83 +1,9 @@
 #!/bin/bash
-#
 
+####################################
+#########install k8s Redhat#########
+####################################
 
-#https://github.com/techiescamp/kubeadm-scripts
-
-# RedHat
-
-# disable swap
-sudo swapoff -a
-(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
-
-sudo yum update -y
-
-# container runtime 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
-br_netfilter
-EOF
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-# sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-
-# Apply sysctl params without reboot
-sudo sysctl --system
-
-lsmod | grep br_netfilter
-lsmod | grep overlay
-
-sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
-
-# Set SELinux in permissive mode (effectively disabling it)
-sudo setenforce 0
-sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-# Install containerd
-
-sudo yum update 
-sudo yum install -y containerd
-echo "containerd runtime installed susccessfully"
-
-# Create a containerd configuration file
-sudo mkdir -p /etc/containerd
-sudo containerd config default | sudo tee /etc/containerd/config.toml
-
-
-# Install kubelet, kubectl and Kubeadm
-
-sudo yum install -y ca-certificates curl
-
-# This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
-enabled=1
-gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
-exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
-EOF
-
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-sudo systemctl enable --now kubelet
-
-
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
-
-
-
-
-###################################################################
 sudo modprobe overlay
 sudo modprobe br_netfilter
 #sudo swapoff –a
@@ -93,7 +19,9 @@ net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-echo '1' > /proc/sys/net/ipv4/ip_forward
+cat <<EOF | sudo tee /proc/sys/net/ipv4/ip_forward
+1
+EOF
 
 #sudo sysctl –system
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
@@ -105,10 +33,10 @@ sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml 
 sudo systemctl restart containerd
 ps -ef | grep containerd
-dnf install -y iproute-tc
+sudo dnf install -y iproute-tc
 
 
-cat < /etc/yum.repos.d/kubernetes.repo
+cat <<EOF | sudo tee  /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -118,22 +46,23 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
-vi /etc/yum.repos.d/kubernetes.repo
+
 
 sudo dnf update -y
 sudo dnf install -y kubelet kubeadm kubectl
-systemctl enable kubelet.service
-sudo hostnamectl set-hostname "master"
-echo "$(hostname -I) master" >> /etc/hosts
+sudo systemctl enable kubelet.service
 
 
-sudo kubeadm init
-systemctl enable kubelet.service
+#sudo hostnamectl set-hostname "master"
+# cat <<EOF | sudo tee -a /etc/hosts
+# "$(hostname -I) master"
+# EOF
+#sudo bash -c 'echo "$(hostname -I) master" >> /etc/hosts'
 
-
-kubeadm init --ignore-preflight-errors=all
-
-
-
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/calico.yaml -O
-sudo kubectl apply -f calico.yaml
+## for master node 
+#sudo kubeadm init
+#kubeadm init --ignore-preflight-errors=all
+#kubeadm token create --print-join-command
+##k8s config file : /etc/kubernetes/admin.conf
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/calico.yaml -O
+#sudo kubectl apply -f calico.yaml
